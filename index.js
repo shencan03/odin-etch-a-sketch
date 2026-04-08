22580256bbce5945f09e1aa288c5d9ab1ca4030a
shencan03 <shencan03@gmail.com>
2026-04-08 18:35:26 +0300

    implement auto column and box width/height to fit fixed container size.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const gridsContainer = document.querySelector(".grids-container")
 
 // helper function that returns css rgb() value
 const getRandomRGB = () => {
-  // get random r-g-b values
+
   const r = Math.round(Math.random() * 256)
   const g = Math.round(Math.random() * 256)
   const b = Math.round(Math.random() * 256) 
@@ -10,12 +10,14 @@ const getRandomRGB = () => {
   return `rgb(${r}, ${g}, ${b})`
 }
 
-for(let i = 0; i < 16; i++){
+const square = 64
+
+for(let i = 0; i < square; i++){
   const col = document.createElement("div")
   col.classList.add("grid-col")
   col.id = i
 
-  for(let x = 0; x < 16; x++){
+  for(let x = 0; x < square; x++){
     const rowItem = document.createElement("div")
     rowItem.classList.add("row-item")
     rowItem.id = x
