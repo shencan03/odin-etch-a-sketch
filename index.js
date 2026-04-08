291f07ba3770f6e175e5cd0e4039f024382d2f8f
shencan03 <shencan03@gmail.com>
2026-04-08 21:18:44 +0300

    implement dynamic size grid allocation where size input via button. fix grids container width and height at constant values.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const gridsContainer = document.querySelector(".grids-container")
 
 // helper function that returns css rgb() value
 const getRandomRGB = () => {
-
   const r = Math.round(Math.random() * 256)
   const g = Math.round(Math.random() * 256)
   const b = Math.round(Math.random() * 256) 
@@ -10,24 +9,37 @@ const getRandomRGB = () => {
   return `rgb(${r}, ${g}, ${b})`
 }
 
-const square = 64
+const renderGrid = (gridSize) => {
 
-for(let i = 0; i < square; i++){
-  const col = document.createElement("div")
-  col.classList.add("grid-col")
-  col.id = i
+  for(let i = 0; i < gridSize; i++){
+    const col = document.createElement("div")
+    col.classList.add("grid-col")
+    col.id = i
 
-  for(let x = 0; x < square; x++){
-    const rowItem = document.createElement("div")
-    rowItem.classList.add("row-item")
-    rowItem.id = x
-    rowItem.addEventListener("mouseenter", () => {
-      rowItem.style.backgroundColor = getRandomRGB()
-    }) 
-    
-    col.appendChild(rowItem)
+    for(let x = 0; x < gridSize; x++){
+      const rowItem = document.createElement("div")
+      rowItem.classList.add("row-item")
+      rowItem.id = x
+      rowItem.addEventListener("mouseenter", () => {
+        rowItem.style.backgroundColor = getRandomRGB()
+      }) 
+      
+      col.appendChild(rowItem)
+    }
+
+    gridsContainer.appendChild(col)
   }
-
-  const gridsContainer = document.querySelector(".grids-container")
-  gridsContainer.appendChild(col)
 }
+
+renderGrid(16)
+
+const sizeButton = document.querySelector(".size-button-container button")
+console.log(sizeButton)
+sizeButton.addEventListener("click", () => {
+  let newGridSize = prompt("Enter new grid size: ")
+  newGridSize = parseInt(newGridSize)
+  if(newGridSize <= 100){
+    gridsContainer.removeChild()
+    renderGrid(newGridSize)
+  }
+})
