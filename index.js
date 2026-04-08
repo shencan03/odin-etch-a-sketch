0f606efc20a4d63af3fa07374b4aa6d7bd9cacc2
shencan03 <shencan03@gmail.com>
2026-04-08 17:44:39 +0300

    implement random backgroundcolor event on gridItem on mouseover

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const gridsContainer = document.querySelector(".grids-container")
 
+// helper function that returns css rgb() value
+const getRandomRGB = () => {
+  // get random r-g-b values
+  const r = Math.round(Math.random() * 256)
+  const g = Math.round(Math.random() * 256)
+  const b = Math.round(Math.random() * 256) 
+
+  return `rgb(${r}, ${g}, ${b})`
+}
+
 for(let i = 0; i < 16; i++){
   const col = document.createElement("div")
   col.classList.add("grid-col")
@@ -9,7 +19,10 @@ for(let i = 0; i < 16; i++){
     const rowItem = document.createElement("div")
     rowItem.classList.add("row-item")
     rowItem.id = x
-
+    rowItem.addEventListener("mouseenter", () => {
+      rowItem.style.backgroundColor = getRandomRGB()
+    }) 
+    
     col.appendChild(rowItem)
   }
 
